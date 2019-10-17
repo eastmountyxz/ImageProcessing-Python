f354189da5d4c9e761ab32d7f027a65f0ae3ee49
eastmountyxz <53172375+eastmountyxz@users.noreply.github.com>
2019-10-17 21:22:22 +0800

    Create blog06-image01.py

---
+++ blog06-resize/blog06-image01.py
@@ -0,0 +1,18 @@
+#encoding:utf-8
+import cv2  
+import numpy as np  
+ 
+#读取图片
+src = cv2.imread('scenery.png')
+
+#图像缩放
+result = cv2.resize(src, (200,100))
+print result.shape
+
+#显示图像
+cv2.imshow("src", src)
+cv2.imshow("result", result)
+
+#等待显示
+cv2.waitKey(0)
+cv2.destroyAllWindows()
