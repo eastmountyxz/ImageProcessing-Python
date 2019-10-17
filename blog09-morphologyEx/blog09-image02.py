d632c87c765a4601617840302154f9b9253ce114
eastmountyxz <53172375+eastmountyxz@users.noreply.github.com>
2019-10-17 22:03:14 +0800

    Add files via upload

---
+++ blog09-morphologyEx/blog09-image02.py
@@ -0,0 +1,20 @@
+#encoding:utf-8
+import cv2  
+import numpy as np  
+
+#读取图片
+src = cv2.imread('test03.png', cv2.IMREAD_UNCHANGED)
+
+#设置卷积核
+kernel = np.ones((10,10), np.uint8)
+
+#图像闭运算
+result = cv2.morphologyEx(src, cv2.MORPH_CLOSE, kernel)
+
+#显示图像
+cv2.imshow("src", src)
+cv2.imshow("result", result)
+
+#等待显示
+cv2.waitKey(0)
+cv2.destroyAllWindows()
