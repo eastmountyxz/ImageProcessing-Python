378610bdb9b3162ccc0f3617da6a5ca9a49c0d28
eastmountyxz <53172375+eastmountyxz@users.noreply.github.com>
2020-11-14 18:28:29 +0800

    Add files via upload

---
+++ blog31-dys/blog31_03.py
@@ -0,0 +1,31 @@
+#encoding:utf-8
+#By:Eastmount CSDN 2020-11-12
+import cv2  
+import numpy as np  
+import matplotlib.pyplot as plt
+
+#读取原始图像
+img = cv2.imread('miao.png')
+
+#获取图像高度和宽度
+height = img.shape[0]
+width = img.shape[1]
+
+#创建一幅图像
+grayimg = np.zeros((height, width, 3), np.uint8)
+
+#图像最大值灰度处理
+for i in range(height):
+    for j in range(width):
+        #获取图像R G B最大值
+        gray = max(img[i,j][0], img[i,j][1], img[i,j][2])
+        #灰度图像素赋值 gray=max(R,G,B)
+        grayimg[i,j] = np.uint8(gray)
+
+#显示图像
+cv2.imshow("src", img)
+cv2.imshow("gray", grayimg)
+
+#等待显示
+cv2.waitKey(0)
+cv2.destroyAllWindows()
