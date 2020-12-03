3b6af985c4bce90ec865bcb4287cc9f45d5cf702
eastmountyxz <53172375+eastmountyxz@users.noreply.github.com>
2020-12-03 12:32:20 +0800

    Add files via upload

---
+++ blog32-Hough_Transform/blog32_11.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+#读取图像
+img = cv2.imread('eyes.png')
+
+#灰度转换
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+#显示原始图像
+plt.subplot(121), plt.imshow(gray, 'gray'), plt.title('Input Image')
+plt.axis('off')
+
+#霍夫变换检测圆
+circles1 = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, 20,
+                            param1=100, param2=30,
+                            minRadius=160, maxRadius=200)
+print(circles1)
+
+#提取为二维
+circles = circles1[0, :, :]
+
+#四舍五入取整
+circles = np.uint16(np.around(circles))
+
+#绘制圆
+for i in circles[:]: 
+    cv2.circle(img, (i[0],i[1]), i[2], (255,0,0), 5) #画圆
+    cv2.circle(img, (i[0],i[1]), 2, (255,0,255), 8) #画圆心
+
+#显示处理图像
+plt.subplot(122), plt.imshow(img), plt.title('Result Image')
+plt.axis('off')
+plt.show()
+
+
+
