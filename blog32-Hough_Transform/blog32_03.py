c0daa0d69006dad2ecfc16c5b2bd7bf6423502d1
eastmountyxz <53172375+eastmountyxz@users.noreply.github.com>
2020-12-03 11:50:45 +0800

    Add files via upload

---
+++ blog32-Hough_Transform/blog32_03.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+import cv2 as cv
+import numpy as np
+from matplotlib import pyplot as plt
+
+#读取图像
+img = cv.imread('Lena.png', 0)
+
+#傅里叶变换
+f = np.fft.fft2(img)
+fshift = np.fft.fftshift(f)
+res = np.log(np.abs(fshift))
+
+#傅里叶逆变换
+ishift = np.fft.ifftshift(fshift)
+iimg = np.fft.ifft2(ishift)
+iimg = np.abs(iimg)
+
+#展示结果
+plt.subplot(131), plt.imshow(img, 'gray'), plt.title('Original Image')
+plt.axis('off')
+plt.subplot(132), plt.imshow(res, 'gray'), plt.title('Fourier Image')
+plt.axis('off')
+plt.subplot(133), plt.imshow(iimg, 'gray'), plt.title('Inverse Fourier Image')
+plt.axis('off')
+plt.show()
+
+rows, cols = img.shape
+crow,ccol = int(rows/2), int(cols/2)
+fshift[crow-30:crow+30, ccol-30:ccol+30] = 0
+
+
+
+
+
