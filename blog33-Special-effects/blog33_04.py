69d2e32f2a84537186c1839c3722ab9ceea6c55f
eastmountyxz <53172375+eastmountyxz@users.noreply.github.com>
2020-12-22 23:28:42 +0800

    Add files via upload

---
+++ blog33-Special-effects/blog33_04.py
@@ -0,0 +1,27 @@
+#coding:utf-8
+#By:Eastmount CSDN 2020-12-22
+import cv2
+import numpy as np
+
+#读取原始图像
+img = cv2.imread('scenery.png')
+
+#图像灰度处理
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+#高斯滤波降噪
+gaussian = cv2.GaussianBlur(gray, (5,5), 0)
+ 
+#Canny算子
+canny = cv2.Canny(gaussian, 50, 150)
+
+#阈值化处理
+ret, result = cv2.threshold(canny, 100, 255, cv2.THRESH_BINARY_INV)
+
+#显示图像
+cv2.imshow('src', img)
+cv2.imshow('result', result)
+
+cv2.waitKey()
+cv2.destroyAllWindows()
+
