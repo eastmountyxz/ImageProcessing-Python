6a2f062d70f625c9080f5444324d6d0c39971c91
eastmountyxz <53172375+eastmountyxz@users.noreply.github.com>
2021-01-13 21:29:19 +0800

    Add files via upload

---
+++ blog34-based-opencv/blog34_03.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8 -*-
+import numpy as np
+
+#定义一维数组
+a = np.array([2, 0, 1, 5, 8, 3])
+print('原始数据:', a)
+
+#输出最大、最小值及形状
+print('最小值:', a.min())
+print('最大值:', a.max())
+print('形状', a.shape)
