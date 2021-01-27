8275eb50e9e3e62422b333f46c063376890ea8c6
eastmountyxz <53172375+eastmountyxz@users.noreply.github.com>
2021-01-27 16:49:23 +0800

    Add files via upload

---
+++ blog35-ljys/blog35_07.py
@@ -0,0 +1,16 @@
+# -*- coding:utf-8 -*-
+import cv2
+import numpy
+
+#读取图片
+img = cv2.imread("Lena.png")
+
+#获取图像形状
+print(img.shape)
+
+#显示图像
+cv2.imshow("Demo", img)
+
+#等待显示
+cv2.waitKey(0)
+cv2.destroyAllWindows()
