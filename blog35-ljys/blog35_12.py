6ebef5e865c808031c3d21f1980f0f269cfe6510
eastmountyxz <53172375+eastmountyxz@users.noreply.github.com>
2021-01-27 16:53:02 +0800

    Add files via upload

---
+++ blog35-ljys/blog35_12.py
@@ -0,0 +1,23 @@
+# -*- coding:utf-8 -*-
+# By: Eastmount CSDN 2021-01-26
+import cv2
+import numpy as np
+
+#读取图片
+img = cv2.imread("Lena.png")
+rows, cols, chn = img.shape
+
+#拆分通道
+b = cv2.split(img)[0]
+
+#设置g、r通道为0
+g = np.zeros((rows,cols), dtype=img.dtype)
+r = np.zeros((rows,cols), dtype=img.dtype)
+
+#合并通道
+m = cv2.merge([b, g, r])
+cv2.imshow("Merge", m)
+           
+#等待显示
+cv2.waitKey(0)
+cv2.destroyAllWindows()
