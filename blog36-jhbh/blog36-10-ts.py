819a77139364d2fbae15fe80dc0d04f237eca180
eastmountyxz <53172375+eastmountyxz@users.noreply.github.com>
2021-02-02 21:12:27 +0800

    Add files via upload

---
+++ blog36-jhbh/blog36-10-ts.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+#By:Eastmount CSDN 2021-02-01
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+#读取图片
+src = cv2.imread('test01.jpg')
+
+#获取图像大小
+rows, cols = src.shape[:2]
+
+#设置图像透视变换矩阵
+pos1 = np.float32([[114, 82], [287, 156], [8, 322], [216, 333]])
+pos2 = np.float32([[0, 0], [188, 0], [0, 262], [188, 262]])
+M = cv2.getPerspectiveTransform(pos1, pos2)
+
+#图像透视变换
+result = cv2.warpPerspective(src, M, (190, 272))
+
+#显示图像
+cv2.imshow("original", src)
+cv2.imshow("result", result)
+
+#等待显示
+cv2.waitKey(0)
+cv2.destroyAllWindows()
