830635494a89da91e74973c10f10ee4ef4eaa15e
eastmountyxz <53172375+eastmountyxz@users.noreply.github.com>
2021-02-02 21:04:14 +0800

    Add files via upload

---
+++ blog36-jhbh/blog36-04-sf.py
@@ -0,0 +1,20 @@
+#encoding:utf-8
+#By:Eastmount CSDN 2021-02-01
+import cv2  
+import numpy as np  
+ 
+#读取图片
+src = cv2.imread('test.bmp')
+rows, cols = src.shape[:2]
+print(rows, cols)
+
+#图像缩放 dsize(列,行)
+result = cv2.resize(src, (int(cols*0.6), int(rows*1.2)))
+
+#显示图像
+cv2.imshow("src", src)
+cv2.imshow("result", result)
+
+#等待显示
+cv2.waitKey(0)
+cv2.destroyAllWindows()
