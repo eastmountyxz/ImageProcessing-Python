38a0e96ce925d4f33ce6a4927199469e203a3ac5
eastmountyxz <53172375+eastmountyxz@users.noreply.github.com>
2021-02-06 20:28:08 +0800

    Update blog01-image02.py

--- blog01-start/blog01-image02.py
+++ blog01-start/blog01-image02.py
@@ -4,18 +4,17 @@ import cv2
 #读取图片
 img = cv2.imread("flower.png", cv2.IMREAD_UNCHANGED)
 test = img[88,142]
-print test
-
+print(test)
 img[88,142] = [255, 255, 255]
-print test
+print(test)
 
 #分别获取BGR通道像素
 blue = img[88,142,0]
-print blue
+print(blue)
 green = img[88,142,1]
-print green
+print(green)
 red = img[88,142,2]
-print red
+print(red)
 
 #显示图像
 cv2.imshow("Demo", img)
