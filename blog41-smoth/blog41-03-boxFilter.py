954510e5b5b06a64cee871b4c372181d0875862a
eastmountyxz <53172375+eastmountyxz@users.noreply.github.com>
2021-06-07 18:37:46 +0800

    Add files via upload

---
+++ blog41-smoth/blog41-03-boxFilter.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# By：Eastmount CSDN 2021-06-07
+import cv2  
+import numpy as np  
+import matplotlib.pyplot as plt
+ 
+#读取图片
+img = cv2.imread('lena.png')
+source = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+ 
+#方框滤波
+result = cv2.boxFilter(source, -1, (3,3), normalize=1)
+
+#用来正常显示中文标签
+plt.rcParams['font.sans-serif']=['SimHei']
+
+#显示图形
+titles = ['原始图像', '方框滤波']  
+images = [source, result]  
+for i in range(2):  
+   plt.subplot(1,2,i+1), plt.imshow(images[i], 'gray')  
+   plt.title(titles[i])  
+   plt.xticks([]),plt.yticks([])  
+plt.show()  
