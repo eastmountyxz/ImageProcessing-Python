1aeb1b7b9ad07223af173a6797865d9791ebe299
eastmountyxz <53172375+eastmountyxz@users.noreply.github.com>
2021-06-07 19:39:08 +0800

    Add files via upload

---
+++ blog41-smoth/blog41-09-noise.py
@@ -0,0 +1,19 @@
+# -*- coding:utf-8 -*-
+import cv2
+import numpy as np
+
+#读取图片
+img = cv2.imread("yn.png", cv2.IMREAD_UNCHANGED)
+rows, cols, chn = img.shape
+
+#加噪声
+for i in range(5000):    
+    x = np.random.randint(0, rows) 
+    y = np.random.randint(0, cols)    
+    img[x,y,:] = 255
+
+cv2.imshow("noise", img)
+           
+#等待显示
+cv2.waitKey(0)
+cv2.destroyAllWindows()
