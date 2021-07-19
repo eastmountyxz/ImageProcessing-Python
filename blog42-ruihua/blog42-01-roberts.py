7b2b7cbf77ceb8e845dd26dea60ff9bc8b9012c4
eastmountyxz <53172375+eastmountyxz@users.noreply.github.com>
2021-07-19 16:20:22 +0800

    Create blog42-01-roberts.py

---
+++ blog42-ruihua/blog42-01-roberts.py
@@ -0,0 +1,34 @@
+#encoding:utf-8
+#By:Eastmount CSDN 2021-07-19
+import cv2  
+import numpy as np  
+import matplotlib.pyplot as plt
+ 
+#读取图像
+img = cv2.imread('lena.png')
+lenna_img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+
+#灰度化处理图像
+grayImage = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+ 
+#Roberts算子
+kernelx = np.array([[-1,0],[0,1]], dtype=int)
+kernely = np.array([[0,-1],[1,0]], dtype=int)
+x = cv2.filter2D(grayImage, cv2.CV_16S, kernelx)
+y = cv2.filter2D(grayImage, cv2.CV_16S, kernely)
+#转uint8 
+absX = cv2.convertScaleAbs(x)      
+absY = cv2.convertScaleAbs(y)    
+Roberts = cv2.addWeighted(absX,0.5,absY,0.5,0)
+
+#用来正常显示中文标签
+plt.rcParams['font.sans-serif']=['SimHei']
+
+#显示图形
+titles = [u'原始图像', u'Roberts算子']  
+images = [lenna_img, Roberts]  
+for i in range(2):  
+   plt.subplot(1,2,i+1), plt.imshow(images[i], 'gray')  
+   plt.title(titles[i])  
+   plt.xticks([]),plt.yticks([])  
+plt.show()
