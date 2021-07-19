0958af6c1829fb7dcd2246e07ca73006afa964f0
eastmountyxz <53172375+eastmountyxz@users.noreply.github.com>
2021-07-19 17:53:03 +0800

    Add files via upload

---
+++ blog42-ruihua/blog42-06-scharr.py
@@ -0,0 +1,31 @@
+#encoding:utf-8
+#By:Eastmount CSDN 2021-07-19
+import cv2  
+import numpy as np  
+import matplotlib.pyplot as plt
+ 
+#读取图像
+img = cv2.imread('lena.png')
+lenna_img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+
+#灰度化处理图像
+grayImage = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+ 
+# Scharr算子
+x = cv2.Scharr(grayImage, cv2.CV_32F, 1, 0) #X方向
+y = cv2.Scharr(grayImage, cv2.CV_32F, 0, 1) #Y方向
+absX = cv2.convertScaleAbs(x)       
+absY = cv2.convertScaleAbs(y)
+Scharr = cv2.addWeighted(absX, 0.5, absY, 0.5, 0)
+
+#用来正常显示中文标签
+plt.rcParams['font.sans-serif']=['SimHei']
+
+#显示图形
+titles = [u'原始图像', u'Scharr算子']  
+images = [lenna_img, Scharr]  
+for i in range(2):  
+   plt.subplot(1,2,i+1), plt.imshow(images[i], 'gray')  
+   plt.title(titles[i])  
+   plt.xticks([]),plt.yticks([])  
+plt.show()
