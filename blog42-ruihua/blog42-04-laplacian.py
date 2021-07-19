9c269a9e8f7cfd6cf75c5d4e55827bb3b741d653
eastmountyxz <53172375+eastmountyxz@users.noreply.github.com>
2021-07-19 16:54:01 +0800

    Add files via upload

---
+++ blog42-ruihua/blog42-04-laplacian.py
@@ -0,0 +1,28 @@
+#encoding:utf-8
+#By:Eastmount CSDN 2021-07-19
+import cv2  
+import numpy as np  
+import matplotlib.pyplot as plt
+ 
+#读取图像
+img = cv2.imread('lena.png')
+lenna_img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+
+#灰度化处理图像
+grayImage = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+ 
+#拉普拉斯算法
+dst = cv2.Laplacian(grayImage, cv2.CV_16S, ksize = 3)
+Laplacian = cv2.convertScaleAbs(dst) 
+
+#用来正常显示中文标签
+plt.rcParams['font.sans-serif']=['SimHei']
+
+#显示图形
+titles = [u'原始图像', u'Laplacian算子']  
+images = [lenna_img, Laplacian]  
+for i in range(2):  
+   plt.subplot(1,2,i+1), plt.imshow(images[i], 'gray')  
+   plt.title(titles[i])  
+   plt.xticks([]),plt.yticks([])  
+plt.show()
