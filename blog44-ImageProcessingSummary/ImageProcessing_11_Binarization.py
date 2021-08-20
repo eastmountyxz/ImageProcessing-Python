1f47c398c01cb8db5969f55e9dbace15f84d3f97
eastmountyxz <53172375+eastmountyxz@users.noreply.github.com>
2021-08-20 17:33:41 +0800

    Add files via upload

---
+++ blog44-ImageProcessingSummary/ImageProcessing_11_Binarization.py
@@ -0,0 +1,25 @@
+#encoding:utf-8
+#By:Eastmount CSDN 2021-08-20
+import cv2  
+import numpy as np  
+import matplotlib.pyplot as plt
+
+#读取图像
+img=cv2.imread('miao.png')
+grayImage=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)  
+
+#阈值化处理
+ret,thresh1=cv2.threshold(grayImage,127,255,cv2.THRESH_BINARY)  
+ret,thresh2=cv2.threshold(grayImage,127,255,cv2.THRESH_BINARY_INV)  
+ret,thresh3=cv2.threshold(grayImage,127,255,cv2.THRESH_TRUNC)  
+ret,thresh4=cv2.threshold(grayImage,127,255,cv2.THRESH_TOZERO)  
+ret,thresh5=cv2.threshold(grayImage,127,255,cv2.THRESH_TOZERO_INV)
+
+#显示结果
+titles = ['Gray Image','BINARY','BINARY_INV','TRUNC','TOZERO','TOZERO_INV']  
+images = [grayImage, thresh1, thresh2, thresh3, thresh4, thresh5]  
+for i in range(6):  
+   plt.subplot(2,3,i+1),plt.imshow(images[i],'gray')  
+   plt.title(titles[i])  
+   plt.xticks([]),plt.yticks([])  
+plt.show()
