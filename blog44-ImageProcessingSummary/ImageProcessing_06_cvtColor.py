e7d5444f27a25b1d9942951daf47f699c20a0900
eastmountyxz <53172375+eastmountyxz@users.noreply.github.com>
2021-08-20 17:30:28 +0800

    Add files via upload

---
+++ blog44-ImageProcessingSummary/ImageProcessing_06_cvtColor.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+# By: Eastmount CSDN 2021-08-20
+import cv2  
+import numpy as np  
+import matplotlib.pyplot as plt
+
+#读取原始图像
+img_BGR = cv2.imread('na.png')
+
+#BGR转换为RGB
+img_RGB = cv2.cvtColor(img_BGR, cv2.COLOR_BGR2RGB)
+
+#灰度化处理
+img_GRAY = cv2.cvtColor(img_BGR, cv2.COLOR_BGR2GRAY)
+
+#BGR转HSV
+img_HSV = cv2.cvtColor(img_BGR, cv2.COLOR_BGR2HSV)
+
+#BGR转YCrCb
+img_YCrCb = cv2.cvtColor(img_BGR, cv2.COLOR_BGR2YCrCb)
+
+#BGR转HLS
+img_HLS = cv2.cvtColor(img_BGR, cv2.COLOR_BGR2HLS)
+
+#BGR转XYZ
+img_XYZ = cv2.cvtColor(img_BGR, cv2.COLOR_BGR2XYZ)
+
+#BGR转LAB
+img_LAB = cv2.cvtColor(img_BGR, cv2.COLOR_BGR2LAB)
+
+#BGR转YUV
+img_YUV = cv2.cvtColor(img_BGR, cv2.COLOR_BGR2YUV)
+
+#调用matplotlib显示处理结果
+titles = ['BGR', 'RGB', 'GRAY', 'HSV', 'YCrCb', 'HLS', 'XYZ', 'LAB', 'YUV']  
+images = [img_BGR, img_RGB, img_GRAY, img_HSV, img_YCrCb,
+          img_HLS, img_XYZ, img_LAB, img_YUV]  
+for i in range(9):  
+   plt.subplot(3, 3, i+1), plt.imshow(images[i], 'gray')  
+   plt.title(titles[i])  
+   plt.xticks([]),plt.yticks([])  
+plt.show()
