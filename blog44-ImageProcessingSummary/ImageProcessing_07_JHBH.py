8d3cee8f6cbfc27edc12b9ec58892a291db20fb0
eastmountyxz <53172375+eastmountyxz@users.noreply.github.com>
2021-08-20 17:30:43 +0800

    Add files via upload

---
+++ blog44-ImageProcessingSummary/ImageProcessing_07_JHBH.py
@@ -0,0 +1,54 @@
+#encoding:utf-8
+#By:Eastmount CSDN 2021-02-01
+import cv2  
+import numpy as np
+import matplotlib.pyplot as plt
+ 
+#读取图片
+img = cv2.imread('test.bmp')
+image = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+
+#图像平移矩阵
+M = np.float32([[1, 0, 100], [0, 1, 80]])
+rows, cols = image.shape[:2]
+img1 = cv2.warpAffine(image, M, (cols, rows))
+
+#图像缩小
+img2 = cv2.resize(image, (200,100))
+
+#图像放大
+img3 = cv2.resize(image, None, fx=1.1, fy=1.1)
+
+#绕图像的中心旋转
+#源图像的高、宽 以及通道数
+rows, cols, channel = image.shape
+#函数参数：旋转中心 旋转度数 scale
+M = cv2.getRotationMatrix2D((cols/2, rows/2), 30, 1) 
+#函数参数：原始图像 旋转参数 元素图像宽高
+img4 = cv2.warpAffine(image, M, (cols, rows))
+
+#图像翻转
+img5 = cv2.flip(image, 0)   #参数=0以X轴为对称轴翻转 
+img6 = cv2.flip(image, 1)   #参数>0以Y轴为对称轴翻转
+
+#图像的仿射
+pts1 = np.float32([[50,50],[200,50],[50,200]])
+pts2 = np.float32([[10,100],[200,50],[100,250]])
+M = cv2.getAffineTransform(pts1,pts2)
+img7 = cv2.warpAffine(image, M, (rows,cols))
+
+#图像的透射
+pts1 = np.float32([[56,65],[238,52],[28,237],[239,240]])
+pts2 = np.float32([[0,0],[200,0],[0,200],[200,200]])
+M = cv2.getPerspectiveTransform(pts1,pts2)
+img8 = cv2.warpPerspective(image,M,(200,200))
+
+
+#循环显示图形
+titles = [ 'source', 'shift', 'reduction', 'enlarge', 'rotation', 'flipX', 'flipY', 'affine', 'transmission']  
+images = [image, img1, img2, img3, img4, img5, img6, img7, img8]  
+for i in range(9):  
+   plt.subplot(3, 3, i+1), plt.imshow(images[i], 'gray')  
+   plt.title(titles[i])  
+   plt.xticks([]),plt.yticks([])  
+plt.show()  
